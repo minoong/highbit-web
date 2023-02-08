9dfefb7d1eb246a702ac9b3c93ef2e1cb44136b5
lmw <alsdndjdrktl@gmail.com>
2023-02-08 21:11:59 +0900

    feat(#3): react-query
    - react-query 프로바이더 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,23 @@
 import GlobalNav from '~/components/GlobalNav/GlobalNav'
+import ReactQueryProvider from '~/components/ReactQueryProiver/ReactQueryProvider'
 import './globals.css'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
  return (
-  <html lang="ko">
+  <html lang="ko-KR">
    {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
    <head />
    <body>
-    <div className="flex min-h-screen w-full min-w-[1400px] flex-col bg-slate-600">
-     <GlobalNav />
-     <main className="flex-auto bg-red-200 px-4 pt-[68px]">{children}</main>
-     <footer>footer</footer>
-    </div>
+    <ReactQueryProvider>
+     <div className="flex min-h-screen w-full min-w-[1400px] flex-col bg-slate-600">
+      <GlobalNav />
+      <main className="flex-auto bg-red-200 px-4 pt-[68px]">{children}</main>
+      <footer>footer</footer>
+     </div>
+    </ReactQueryProvider>
    </body>
   </html>
  )
