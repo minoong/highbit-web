15082c88655c4a4d2a37936cf1f156b291d52105
lmw <alsdndjdrktl@gmail.com>
2023-02-18 20:28:53 +0900

    feat(#25): WebSocket을 이용한 업비트 시세 수신
    - 업비트 Websocket을 이용한 현재가, 채결 그리고 호가 데이터 수신 커스텀 훅 구현
    - 많은 양의 데이터를 처리해야됨에 따라 버퍼, 쓰로틀을 이용하여 데이터 정제
    - Webcokset 오류 발생 시 재연결 로직 구현

---
+++ src/hooks/useUpbit.websocket.ts
@@ -0,0 +1,158 @@
+import type { Market } from '~/types/apis/market'
+import type { Orderbook } from '~/types/apis/orderbook.socket'
+import type { Ticker } from '~/types/apis/ticker.socket'
+import type { Trade } from '~/types/apis/trade.socket'
+import { cloneDeep, throttle } from 'lodash'
+import { useCallback, useEffect, useRef, useState } from 'react'
+
+type RequestType = 'ticker' | 'orderbook' | 'trade'
+
+type ResultInfer<T> = T extends 'ticker' ? Ticker : T extends 'orderbook' ? Orderbook : Trade
+
+function socketDataEncoder(socketData: Iterable<number>) {
+ const encoder = new TextDecoder('utf-8')
+ const rawData = new Uint8Array(socketData)
+ const data = JSON.parse(encoder.decode(rawData))
+
+ return data
+}
+
+function getLastBuffers<T extends { code: string }>(buffer: T[], bufferSize: number) {
+ return cloneDeep(buffer)
+  .reverse()
+  .reduce((prev, curr, _, arr) => {
+   const isExist = prev.some((value) => value.code === curr.code)
+   if (!isExist) {
+    return [...prev, curr]
+   }
+   if (bufferSize <= prev.length) arr.slice(1)
+   return prev
+  }, [] as T[])
+}
+
+function sortBuffers<T extends { code: string }>(buffer: T[], marketCodes: Market[]) {
+ return marketCodes.reduce(
+  (prev, curr) => [...prev, buffer.find((v) => v.code === curr.market)].filter((v) => v) as T[],
+  [] as T[],
+ )
+}
+
+function updateSocketData<T extends { code: string }>(originData: T[], newData: T[]) {
+ const data = cloneDeep(originData).map((acc) => {
+  const search = newData.filter((v) => v.code === acc.code).at(-1)
+
+  return search || acc
+ })
+
+ return [...data, ...newData.filter((value) => data.every((matchedData) => matchedData.code !== value.code))]
+}
+
+function updateQueueBuffer<T extends { code: string }>(buffer: T[], maxSize: number) {
+ return [...buffer].slice(-maxSize).reverse()
+}
+
+function useUpbit<T extends RequestType>(marketCodes: Market[], type: T) {
+ const socket = useRef<WebSocket | null>(null)
+ const buffer = useRef<ResultInfer<T>[]>([])
+
+ const [isConnected, setIsConnected] = useState<boolean>(false)
+ const [loadingBuffer, setLoadingBuffer] = useState<ResultInfer<T>[]>([])
+ const [socketData, setSocketData] = useState<ResultInfer<T>[]>([])
+
+ const throttled = useCallback(
+  throttle(() => {
+   if (type === 'ticker' || type === 'orderbook') {
+    const lastBuffers = getLastBuffers(buffer.current, marketCodes.length)
+
+    if (type === 'ticker') {
+     const sortedBuffers = sortBuffers(lastBuffers, marketCodes)
+     setLoadingBuffer(sortedBuffers)
+     buffer.current = []
+    } else {
+     if (lastBuffers) setSocketData([lastBuffers[0]])
+     buffer.current = []
+    }
+   } else {
+    const updatedBuffer = updateQueueBuffer(buffer.current, 100)
+    buffer.current = updatedBuffer
+    setSocketData(updatedBuffer)
+   }
+  }, 400),
+  [marketCodes],
+ )
+
+ useEffect(() => {
+  function connect() {
+   socket.current = new WebSocket('wss://api.upbit.com/websocket/v1')
+   socket.current.binaryType = 'arraybuffer'
+
+   socket.current.onopen = () => {
+    setIsConnected(true)
+
+    if (socket.current?.readyState === 1) {
+     const payload = [
+      { ticket: type },
+      {
+       type,
+       codes: marketCodes.map(({ market }) => market),
+      },
+     ]
+     socket.current.send(JSON.stringify(payload))
+    }
+   }
+
+   socket.current.onmessage = (ev) => {
+    const data = socketDataEncoder(ev.data)
+    buffer.current?.push(data)
+    throttled()
+   }
+
+   socket.current.onclose = () => {
+    setIsConnected(false)
+    setLoadingBuffer([])
+    setSocketData([])
+    buffer.current = []
+   }
+
+   socket.current.onerror = () => {
+    socket.current?.close()
+
+    setTimeout(() => {
+     connect()
+    }, 400)
+   }
+  }
+
+  connect()
+
+  return () => {
+   if (socket.current && socket.current.readyState !== 0) {
+    socket.current.close()
+    socket.current = null
+    setIsConnected(false)
+    setLoadingBuffer([])
+    setSocketData([])
+    buffer.current = []
+   }
+  }
+ }, [marketCodes, throttled, type])
+
+ useEffect(() => {
+  if (loadingBuffer.length < 1) {
+   return
+  }
+
+  if (!socketData) {
+   setSocketData(loadingBuffer)
+  } else {
+   setSocketData((prev) => {
+    return updateSocketData(prev, loadingBuffer)
+   })
+   setLoadingBuffer([])
+  }
+ }, [loadingBuffer, socketData])
+
+ return { socket: socket.current, isConnected, socketData }
+}
+
+export default useUpbit
