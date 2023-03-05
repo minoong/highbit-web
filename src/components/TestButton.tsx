dfbda61b25ff06efc0ec64c12fd28c213d0072ec
lmw <alsdndjdrktl@gmail.com>
2023-03-05 18:03:49 +0900

    feat(#55): Tabs 컴포넌트
    - Chakra UI installed
    - 원화/보유/관신 Tabs 컴포넌트

--- src/components/TestButton.tsx
+++ src/components/TestButton.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { CacheProvider } from '@chakra-ui/next-js'
+import { ChakraProvider, Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react'
 import { useEffect, useState } from 'react'
 import { styled } from 'styled-components'
 import TickerBox from '~/components/ui/Ticker/TickerBox'
@@ -45,26 +47,60 @@ function TestButton() {
  }, [dispatch, socketData])
 
  return (
-  <Blocked>
-   <div>
-    <div className="virtualscroll sticky top-20 h-[993px]" onScroll={(e) => setY(e.currentTarget.scrollTop)}>
-     <TickerSearch />
-     <TickerSort />
-     <VirtualScroll height={993} itemHeight={46} offsetY={y}>
-      {tickers
-       .filter((v) => v.market.startsWith('KRW-'))
-       .map((ticker) => (
-        <TickerBox
-         key={ticker.market}
-         {...ticker}
-         market={markets.find((v) => v.market === ticker.market)?.market || ''}
-         korean_name={markets.find((v) => v.market === ticker.market)?.korean_name || ''}
-        />
-       ))}
-     </VirtualScroll>
-    </div>
-   </div>
-  </Blocked>
+  <CacheProvider>
+   <ChakraProvider>
+    <Blocked>
+     <div>
+      <div className="sticky top-20 h-[959px]">
+       <TickerSearch />
+
+       <Tabs isFitted size="sm">
+        <TabList>
+         <Tab>
+          <div className="font-extrabold hover:underline">원화</div>
+         </Tab>
+         <Tab>
+          <div className="font-extrabold hover:underline">보유</div>
+         </Tab>
+         <Tab>
+          <div className="font-extrabold hover:underline">관심</div>
+         </Tab>
+        </TabList>
+        <TabPanels>
+         <TabPanel padding={0}>
+          <TickerSort />
+          <div className="virtualscroll sticky h-[865px]" onScroll={(e) => setY(e.currentTarget.scrollTop)}>
+           <VirtualScroll height={865} itemHeight={46} offsetY={y}>
+            {tickers
+             .filter((v) => v.market.startsWith('KRW-'))
+             .map((ticker) => (
+              <TickerBox
+               key={ticker.market}
+               {...ticker}
+               market={markets.find((v) => v.market === ticker.market)?.market || ''}
+               korean_name={markets.find((v) => v.market === ticker.market)?.korean_name || ''}
+              />
+             ))}
+           </VirtualScroll>
+          </div>
+         </TabPanel>
+         <TabPanel>
+          <div className="mt-20 flex h-full items-center justify-center text-xs font-semibold text-neutral-700/70">
+           로그인하면 내 보유자산을 확인할 수 있습니다.
+          </div>
+         </TabPanel>
+         <TabPanel>
+          <div className="mt-20 flex h-full items-center justify-center text-xs font-semibold text-neutral-700/70">
+           로그인하면 내 관심코인을 확인할 수 있습니다.
+          </div>
+         </TabPanel>
+        </TabPanels>
+       </Tabs>
+      </div>
+     </div>
+    </Blocked>
+   </ChakraProvider>
+  </CacheProvider>
  )
 }
 
