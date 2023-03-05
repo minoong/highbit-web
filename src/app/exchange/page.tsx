61e213ce7735841e0bcc71901bdab9b2e2766198
lmw <alsdndjdrktl@gmail.com>
2023-03-05 18:34:24 +0900

    hotfix(#57): Chakra UI provider 위치 변경
    - Chakra UI 사용 시 Next.js 13 App 디렉터리 버전의 경우 page.tsx에 provider를 지정

--- src/app/exchange/page.tsx
+++ src/app/exchange/page.tsx
@@ -1,28 +1,51 @@
+'use client'
+
+import { CacheProvider } from '@chakra-ui/next-js'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import { Suspense } from 'react'
 import { MinutesStockChart } from '~/components/ui/CandleChart/stockChart'
 import CoinHeader from '~/components/ui/CoinHeader/CoinHeader'
 import OrderBookContainer from '~/components/ui/OrderBook/OrderBookContainer'
 import TestButton from '~/components/TestButton'
 
+const theme = extendTheme({
+ styles: {
+  global: () => ({
+   body: {
+    color: 'default',
+    bg: '#e9ecf1',
+   },
+  }),
+ },
+})
+
+export const metadata = {
+ title: 'asdfjklsdjflk',
+}
+
 function ExchangePage() {
  return (
-  <div className="m-auto mt-4 grid w-[1410px] grid-cols-[990px_400px] gap-5">
-   <div className="flex flex-col gap-2">
-    <CoinHeader />
-    <div className="h-[500px] bg-white">
-     <MinutesStockChart dateTimeFormat="%Y-%m-%d %H:%M" />
+  <CacheProvider>
+   <ChakraProvider theme={theme}>
+    <div className="m-auto mt-4 grid w-[1410px] grid-cols-[990px_400px] gap-5">
+     <div className="flex flex-col gap-2">
+      <CoinHeader />
+      <div className="h-[500px] bg-white">
+       <MinutesStockChart dateTimeFormat="%Y-%m-%d %H:%M" />
+      </div>
+      <Suspense fallback={<div>Loading...</div>}>
+       <OrderBookContainer />
+      </Suspense>
+      <article className="h-[436px] bg-white">주문</article>
+     </div>
+     <div className="sticky top-20 h-[993px] bg-white">
+      <Suspense fallback={<div>Loading...</div>}>
+       <TestButton />
+      </Suspense>
+     </div>
     </div>
-    <Suspense fallback={<div>Loading...</div>}>
-     <OrderBookContainer />
-    </Suspense>
-    <article className="h-[436px] bg-white">주문</article>
-   </div>
-   <div className="sticky top-20 h-[993px] bg-white">
-    <Suspense fallback={<div>Loading...</div>}>
-     <TestButton />
-    </Suspense>
-   </div>
-  </div>
+   </ChakraProvider>
+  </CacheProvider>
  )
 }
 
