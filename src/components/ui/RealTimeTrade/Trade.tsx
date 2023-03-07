6b16ff5b322bd1a6c8c6f744ed218985d4429204
lmw <alsdndjdrktl@gmail.com>
2023-03-07 21:47:18 +0900

    feat(#61): realtime 체결 컴포넌트
    - upbit websocket Trade API 연동

---
+++ src/components/ui/RealTimeTrade/Trade.tsx
@@ -0,0 +1,32 @@
+import type { Trade } from '~/types/apis/trade.socket'
+import clsx from 'clsx'
+import dayjs from 'dayjs'
+import React from 'react'
+import { MarketUtils } from '~/utils/marketUtils'
+
+interface Props {
+ data: Trade
+}
+
+function Trade(props: Props) {
+ const {
+  data: { ask_bid, change, trade_price, trade_volume, trade_timestamp },
+ } = props
+
+ const currentChange = MarketUtils.getChageColor('text-', change, '[#333333]')
+ const askBid = `${clsx(ask_bid === 'BID' && 'text-trade-rise', ask_bid === 'ASK' && 'text-trade-fall')}`
+
+ return (
+  <div className="flex h-[35px] w-fit items-center text-right text-[11px] text-[#666666] even:bg-[#F9FAFC]">
+   <div className="w-[96px] text-center">
+    <span className="text-[#333333]">{dayjs(trade_timestamp).format('MM.DD')}</span>
+    <span className={`text-[#999999] before:content-['_']`}>{dayjs(trade_timestamp).format('HH.mm')}</span>
+   </div>
+   <div className={`w-[300px] ${currentChange} font-medium`}>{MarketUtils.getPricePretty(trade_price)}</div>
+   <div className={`w-[280px] ${askBid}`}>{trade_volume}</div>
+   <div className="w-[314px] pr-[14px] font-medium">{MarketUtils.getPricePretty(trade_price * trade_volume)}</div>
+  </div>
+ )
+}
+
+export default Trade
---
+++ src/types/apis/tick.ts
@@ -0,0 +1,12 @@
+export type Tick = {
+ market: string
+ trade_date_utc: string
+ trade_time_utc: string
+ timestamp: number
+ trade_price: number
+ trade_volume: number
+ prev_closing_price: number
+ change_price: number
+ ask_bid: string
+ sequential_id: number
+}
