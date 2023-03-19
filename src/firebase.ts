8b9b9e1f77ff9f60b4962cc80bbdc9806a39524a
lmw <alsdndjdrktl@gmail.com>
2023-03-19 21:27:34 +0900

    feat(#69): 관심 코인 즐겨찾기 with firestore
    - firebase 스냅샷
    - 북마크 커스텀 훅 구현

---
+++ src/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+ apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY as string,
+ authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN as string,
+ projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID as string,
+ storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET as string,
+ messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID as string,
+ appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID as string,
+}
+
+const app = initializeApp(firebaseConfig)
+const db = getFirestore()
+
+export { app, db }
---
+++ src/hooks/useCoinBookMark.ts
@@ -0,0 +1,79 @@
+import {
+ arrayRemove,
+ arrayUnion,
+ collection,
+ doc,
+ documentId,
+ onSnapshot,
+ query,
+ setDoc,
+ where,
+} from 'firebase/firestore'
+import { useSession } from 'next-auth/react'
+import { useCallback, useEffect, useState } from 'react'
+import { db } from '~/firebase'
+
+export function useCoinBookMark() {
+ const { data: session, status } = useSession()
+ const [coinBookMarkList, setCoinBookMarkList] = useState<string[]>([])
+
+ useEffect(() => {
+  if (status !== 'authenticated' || !session?.user?.name) return
+
+  const unsubscribe = onSnapshot(
+   query(collection(db, 'bookmark'), where(documentId(), '==', session.user.name)),
+   (snapshot) => {
+    if (snapshot.empty) return
+
+    const result = snapshot.docs[0].data().list as string[]
+    setCoinBookMarkList(result)
+   },
+  )
+
+  return () => unsubscribe()
+ }, [session?.user?.name, status])
+
+ const addCoinBookMark = useCallback(
+  async (coin: string) => {
+   if (!session?.user?.name) return false
+
+   try {
+    await setDoc(
+     doc(db, 'bookmark', session.user.name),
+     {
+      list: arrayUnion(coin),
+     },
+     { merge: true },
+    )
+
+    return true
+   } catch {
+    return false
+   }
+  },
+  [session?.user?.name],
+ )
+
+ const removeCoinBookMark = useCallback(
+  async (coin: string) => {
+   if (!session?.user?.name) return false
+
+   try {
+    await setDoc(
+     doc(db, 'bookmark', session.user.name),
+     {
+      list: arrayRemove(coin),
+     },
+     { merge: true },
+    )
+
+    return true
+   } catch {
+    return false
+   }
+  },
+  [session?.user?.name],
+ )
+
+ return [coinBookMarkList, addCoinBookMark, removeCoinBookMark] as const
+}
