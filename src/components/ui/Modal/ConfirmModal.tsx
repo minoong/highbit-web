da759c6cc952ad915ef808c45557a34c64b3c0cb
lmw <alsdndjdrktl@gmail.com>
2023-03-25 20:34:51 +0900

    feat(#73): Confirm 모달
    - 코어 모달 컴포넌트
    - 컴펌 모달 컴포넌트

---
+++ src/components/ui/Modal/ConfirmModal.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import Modal from '~/components/ui/Modal/Modal'
+import { closeModal } from '~/features/modals/ModalsSlice'
+import { useAppDispatch } from '~/hooks'
+
+interface Props {
+ title: React.ReactNode
+ contents: React.ReactNode
+ label?: string
+ closeLabel?: string
+ onConfirmClick?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => Promise<any>
+ onCloseClick?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => Promise<any>
+}
+
+function ConfirmModal(props: Props) {
+ const { title, contents, label = '확인', closeLabel = '취소', onConfirmClick, onCloseClick } = props
+
+ const dispatch = useAppDispatch()
+
+ const handleConfirm = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  if (onConfirmClick) {
+   await onConfirmClick(e)
+  }
+
+  dispatch(closeModal({ Component: ConfirmModal }))
+ }
+
+ const handleClose = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  if (onCloseClick) {
+   await onCloseClick(e)
+  }
+
+  dispatch(closeModal({ Component: ConfirmModal }))
+ }
+
+ return (
+  <Modal
+   height={500}
+   title={title}
+   contents={contents}
+   footer={
+    <div className="flex w-full justify-end gap-1 text-lg">
+     <button
+      className="rounded-sm bg-white px-8 text-blue-700/90 ring-1 ring-blue-700/90 hover:brightness-125"
+      onClick={handleClose}
+     >
+      {closeLabel}
+     </button>
+     <button
+      className="rounded-sm bg-blue-700/90 px-8 text-white ring-1 ring-blue-700/90 hover:brightness-125"
+      onClick={handleConfirm}
+     >
+      {label}
+     </button>
+    </div>
+   }
+  />
+ )
+}
+
+export default ConfirmModal
