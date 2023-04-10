2530b93ab9b82c4f7d4fbec5c79d3ba3d71a8b7a
lmw <alsdndjdrktl@gmail.com>
2023-04-10 20:55:10 +0900

    feat(#83): Range Slider 컴포넌트
    - RangeSlider 컴포넌트 구현

---
+++ src/components/RangeSlider/RangeSlider.stories.tsx
@@ -0,0 +1,18 @@
+import type { ComponentMeta, ComponentStory } from '@storybook/react'
+import React from 'react'
+import RangeSlider from '~/components/RangeSlider/RangeSlider'
+
+import '../../app/globals.css'
+
+export default {
+ title: 'components/RangeSlider',
+ component: RangeSlider,
+} as ComponentMeta<typeof RangeSlider>
+
+const Template: ComponentStory<typeof RangeSlider> = (args) => <RangeSlider {...args} />
+
+export const Primary = Template.bind({})
+
+Primary.args = {
+ change: (value) => console.log(value),
+}
