9e60f3552e0bbcce95006f3bae30cd81b9aa20d6
lmw <alsdndjdrktl@gmail.com>
2023-04-29 11:22:59 +0900

    chore: nextjs 13.3 업데이트
    - nextjs 13.3 updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
+import type { Metadata } from 'next'
 import InteractiveCard from '~/components/InteractiveCard/InteractiveCard'
+
+export const metadata: Metadata = {
+ title: 'highbit',
+ description: 'highbit',
+}
 
 export default function Home() {
  return (
